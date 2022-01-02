2757a1cbd066407896caebebf785b16f6879ac2a
coltroyal1 <91559902+coltroyal1@users.noreply.github.com>
2022-01-02 16:54:07 -0500

    Add files via upload

---
+++ adding.js
@@ -0,0 +1,45 @@
+let add = function(num1, num2) {
+    return num1 + num2;
+}
+console.log("Adding");
+console.log(add(5, 10));
+console.log(add(3000, 210));
+
+let subtract = function(num1, num2) {
+    return num1 - num2;
+}
+console.log("subtracting");
+console.log(subtract(5, 10));
+console.log(subtract(3000, 210));
+
+let multiply = function(num1, num2) {
+    return num1 * num2;
+}
+console.log("multiplying");
+console.log(multiply(5, 10));
+console.log(multiply(3000, 210));
+
+let divide = function(num1, num2) {
+    return num1 / num2;
+}
+console.log("divideing");
+console.log(divide(5, 10));
+console.log(divide(3000, 210));
+
+let array = new array;
+
+
+let a = [1,2,3];
+let b = ["x","y","z"];
+let c = [a,b];
+console.log(a,b,c);
+
+let o = new Object;
+
+
+o.name = "isaiah";
+o.age = 100
+
+cl( o);
+
+console.table(o)
