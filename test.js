c0607fc1cc04f2adf9de03f84393cf4799f96ede
ColtRoyal <isaiah.torresmorrow@gmai.com>
2022-01-02 20:13:08 -0500

    test

---
+++ test.js
@@ -0,0 +1,16 @@
+let a = 2;
+a -= 1;
+a++;
+let b = 8;
+b += 2;
+// 130
+const c = a + b * b;
+// 40
+const d = a * b + b;  
+// 60
+const e = a * (b + b);
+// 10
+const f = a * b / a;
+// 10
+const g = b / a * a;
+console.log(a, b, c, d, e, f, g);
